05a0209557f8d185410217b17d6808b82d4377b4
Unknown <electronicplayground051@gmail.com>
2018-03-15 15:50:47 +0200

    add pir and IR remote control to LinkistStation

--- LinkitSmartDuo_Station/LinkitSmartDuo_Station.ino
+++ LinkitSmartDuo_Station/LinkitSmartDuo_Station.ino
@@ -1,6 +1,7 @@
 #include "IRLibAll.h"
 #include <string.h>
 #include <stdlib.h>
+#include "Timer.h"
 
 //serial msg: node;cmd;payload
 
@@ -8,10 +9,18 @@
 #define TOKEN_COUNT 3
 
 
+char tokens[TOKEN_COUNT][JSON_BUFFER_SIZE/TOKEN_COUNT];
+long code;
+int inputPin = 6;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;       
+String node, cmd, payload;
+
 //Create a sender
 IRsend mySender; 
+Timer t;
 
-void Tokenize(String msg, char* delimiter, char **tokens, short* tokenCount);
+void Tokenize(String msg, char* delimiter, char tokens[TOKEN_COUNT][JSON_BUFFER_SIZE/TOKEN_COUNT], short* tokenCount);
 void HandleIR(char payload[JSON_BUFFER_SIZE/TOKEN_COUNT]);
 
  
@@ -20,43 +29,67 @@ void setup() {
   Serial1.begin(57600);
   delay(2000);
   Serial1.println(F("Ready"));
+
+  t.every(300, HandlePIR);
 }
  
 void loop() {
    ReadSerial();
-   
+   t.update();
+   //HandlePir();
 }
 
 void ParseMessage(String msg) {
- char tokens[TOKEN_COUNT][JSON_BUFFER_SIZE/TOKEN_COUNT];
- char** p = tokens;
- short tokenCount;
- Tokenize(msg,";",p, &tokenCount);
+  short tokenCount;
+  Tokenize(msg,";",tokens, &tokenCount);
 
- for (short i=0; i<tokenCount;i++) {
+  /*for (short i=0; i<tokenCount;i++) {
    Serial1.println(tokens[i]);
- }
+  }
+*/
+  node = String(tokens[0]);
+  cmd = String(tokens[1]);
+  payload = String(tokens[2]);
 
- if (strcmp(tokens[0], "ir")) {
-   HandleIR(tokens[2]);
- }
+  if (node.equals("ir")) {
+   HandleIR(payload);
+  } else if (node.equals("display")) {
+    HandleDisplay(cmd, payload);
+  }
 
 }
 
 
-void HandleIR(char payload[JSON_BUFFER_SIZE/TOKEN_COUNT]) {
-   Serial1.println("IR message");
-    char codes[3][10];
-    short tokensCount;
+void HandleIR(String payload) {
+  code = GetCodeFromHex(payload);
+ 
+  mySender.send(7, code, 32);
+}
 
-    Tokenize(String(payload), ",", codes, &tokensCount);
-   
-    for (short i=0; i<tokensCount; i++) {
-      long code = GetCodeFromHex(tokens[i]);
-      mySender.send(7, code, 32);
-      delay(200);
-      Serial1.println("sent");
+void HandleDisplay(String cmd, String payload) {
+  Serial1.println("handle display");
+  Serial1.println(cmd);
+  Serial1.println(payload);
+}
+
+void HandlePIR() {
+  val = digitalRead(inputPin);  // read input value
+  if (val == HIGH) {            // check if the input is HIGH
+  
+    if (pirState == LOW) {
+      // we have just turned on
+      Serial1.println("pir;1");
+      // We only want to print on the output change, not state
+      pirState = HIGH;
     }
+  } else {
+    if (pirState == HIGH){
+      // we have just turned off
+      Serial1.println("pir;0");
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
+  }
 }
 
 long GetCodeFromHex(String s) {
@@ -83,15 +116,14 @@ long GetCodeFromHex(String s) {
 void ReadSerial(){
   while (Serial1.available()){
     String msg = Serial1.readString();
-    delay(100);
- 
+
     ParseMessage(msg);
   }
 }
 
 
-  
-void Tokenize(String msg, char* delimiter, char** tokens, short* tokenCount)
+
+void Tokenize(String msg, char* delimiter, char tokens[TOKEN_COUNT][JSON_BUFFER_SIZE/TOKEN_COUNT], short* tokenCount)
 {
   char *s = new char[msg.length()+1];
   msg.toCharArray(s, msg.length()+1);
